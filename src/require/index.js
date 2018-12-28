7aee06b82d0020ce8ccf74ba88aa26340ceb93ed
bobo1104839668 <1104839668@qq.com>
2018-12-28 09:44:10 +0800

    构建首页静态布局

--- src/require/index.js
+++ src/require/index.js
@@ -3,12 +3,13 @@ require.config({
 	"paths" : {
 		"jquery" : "jquery-1.11.3",
 		"cookie" : "jquery-cookie"
+		// "bootstrop" : "bootstrop"
 	}
 })
 
 //导入
 require(['jquery','cookie'],function($,cookie){
 	$(function(){
-		
+		// $('body').append("<h1>一级标题</h1>")
 	})
 })
