73998dbbe989457723ee76c5f3dbd6eac4d36d67
bobo1104839668 <1104839668@qq.com>
2019-01-02 21:23:35 +0800

    列表页动态完成，详情页完成

--- src/js/index.js
+++ src/js/index.js
@@ -9,20 +9,29 @@ require.config({
 		"login" : "login",
 		"cookies" : "cookie",
 		"registor" : "registor",
-		"slide1" : "slide1"
+		"slide1" : "slide1",
+		"mark" : "bigGlass",
+		"buy" : "buy"
 	}
 })
 
 //导入
-require(['jquery','cookie',"slide","nav","sample","login","cookies","registor","slide1"],function($,cookie,slide,nav,sample,login,cookies,registor,slide1){
+require(['jquery','cookie',"slide","nav","sample","login","cookies","registor","slide1","mark","buy"],function($,cookie,slide,nav,sample,login,cookies,registor,slide1,mark,buy){
 	$(function(){
 		let section = $("div.banner").children()[0];
 		let sections = $("#ps");
+		//首页轮播动画
 // 		slide(section);
 		// slide1(sections);
 		// slide1($("#ps1"));
+		//导航菜单
 		nav.nav("ul");
+		//首页苹果框动画
 		sample.sample("sample");
+		//查看商品（放大镜）
+		mark.mark("big");
+		//详情添加购物车
+		buy.buy();
 		registor.registor();
 		if(login){
 			location.href = "../html/index.html";
